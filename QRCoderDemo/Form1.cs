ee2f5640842b02537741ddd5229cb4243194febc
zhangbo9 <zhangbo9@lenovo.com>
2015-09-16 16:10:50 +0800

    update demo for add icon

--- QRCoderDemo/Form1.cs
+++ QRCoderDemo/Form1.cs
@@ -34,7 +34,45 @@ namespace QRCoderDemo
             QRCodeGenerator.ECCLevel eccLevel = (QRCodeGenerator.ECCLevel)(level == "L" ? 0 : level == "M" ? 1 : level == "Q" ? 2 : 3);
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(textBoxQRCode.Text, eccLevel);
-            pictureBoxQRCode.BackgroundImage = qrCode.GetGraphic(20);
+
+            pictureBoxQRCode.BackgroundImage = qrCode.GetGraphic(20, Color.Black, Color.White, getIconBitmap());
+        }
+
+        private Bitmap getIconBitmap()
+        {
+            Bitmap img = null;
+            if (iconPath.Text.Length > 0)
+            {
+                try
+                {
+                    img = new Bitmap(iconPath.Text);
+                }
+                catch (Exception) 
+                { 
+                }
+            }
+            return img;
+        }
+
+        private void selectIconBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.Title = "Select icon";
+            openFileDlg.Multiselect = false;
+            openFileDlg.CheckFileExists = true;
+            DialogResult dr = openFileDlg.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                iconPath.Text = openFileDlg.FileName;
+                if (iconSize.Value == 0)
+                {
+                    iconSize.Value = 15;
+                }
+            }
+            else
+            {
+                iconPath.Text = "";
+            }
         }
     }
 }
