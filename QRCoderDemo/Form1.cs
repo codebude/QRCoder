a6b3a5acb314e060a4b0caa910da2c6c69a25dc6
zizwiz <zizwiz@engineer.com>
2016-04-12 20:35:14 +0100

    Update Form1.cs
    Added button so I could save bitmaps

--- QRCoderDemo/Form1.cs
+++ QRCoderDemo/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using QRCoder;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace QRCoderDemo
 {
@@ -41,6 +43,12 @@ namespace QRCoderDemo
 
                         pictureBoxQRCode.BackgroundImage = qrCode.GetGraphic(20, Color.Black, Color.White,
                             GetIconBitmap(), (int) iconSize.Value);
+
+                         this.pictureBoxQRCode.Size = new System.Drawing.Size(pictureBoxQRCode.Width, pictureBoxQRCode.Height);
+                        //Set the SizeMode to center the image.
+                        this.pictureBoxQRCode.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                        pictureBoxQRCode.SizeMode = PictureBoxSizeMode.StretchImage;
                     }
                 }
             }
@@ -80,6 +88,71 @@ namespace QRCoderDemo
             {
                 iconPath.Text = "";
             }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+
+            // Displays a SaveFileDialog so the user can save the Image
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|JPeg Image|*.jpg|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+
+                using (var bitmap = new Bitmap(pictureBoxQRCode.Width, pictureBoxQRCode.Height))  //  pictureBoxQRCode.BackgroundImage.Width, pictureBoxQRCode.BackgroundImage.Height))
+                {
+                    pictureBoxQRCode.DrawToBitmap(bitmap, ClientRectangle);
+                    ImageFormat imageFormat = null;
+
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            imageFormat = ImageFormat.Bmp;
+                            break;
+                        case 2:
+                            imageFormat = ImageFormat.Png;
+                            break;
+                        case 3:
+                            imageFormat = ImageFormat.Jpeg;
+                            break;
+                        case 4:
+                            imageFormat = ImageFormat.Gif;
+                            break;
+                        default:
+                            throw new NotSupportedException("File extension is not supported");
+                    }
+
+                    bitmap.Save(fs, imageFormat);
+                }
+
+
+
+                fs.Close();
+            }
+
+
+
+
+
+        }
+
+        public void ExportToBmp(string path)
+        {
+            
         }
     }
 }
