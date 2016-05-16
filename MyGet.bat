2f3c804aafbc48fc8fff5b7132789b4640b4a2fc
Raffael Herrmann <netblognet@googlemail.com>
2016-05-16 18:40:26 +0200

    Updated build scripts

--- MyGet.bat
+++ MyGet.bat
@@ -9,7 +9,6 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild" QRCoder\QRCoder.csproj /p:Configuration="%config%";VisualStudioVersion=14.0 /tv:14.0 /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
