8d0cb54907819a2f32d5e466fbb7dd5f21e53e44
codebude <netblognet@googlemail.com>
2016-05-20 08:36:28 +0200

    Added category extension for xUnit-Tests
    Added option to add "Category"-attribute to xUnit tests, so that tests
    can be grouped (for example in MS Unit Test Explorer).

---
+++ QRCoderTests/CategoryDiscoverer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace QRCoderTests.XUnitExtenstions
+{
+    public class CategoryDiscoverer : ITraitDiscoverer
+    {
+        public const string KEY = "Category";
+
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            var ctorArgs = traitAttribute.GetConstructorArguments().ToList();
+            yield return new KeyValuePair<string, string>(KEY, ctorArgs[0].ToString());
+        }
+    }
+
+    //NOTICE: Take a note that you must provide appropriate namespace here
+    [TraitDiscoverer("QRCoderTests.XUnitExtenstions.CategoryDiscoverer", "QRCoderTests")]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class CategoryAttribute : Attribute, ITraitAttribute
+    {
+        public CategoryAttribute(string category) { }
+    }
+}
