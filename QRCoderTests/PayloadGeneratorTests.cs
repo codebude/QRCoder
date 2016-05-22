c5abb963af0b2947d1911e27c125763388176a85
Raffael Herrmann <netblognet@googlemail.com>
2016-05-22 13:42:13 +0200

    Added unit tests
    New unit tests in for the payload generator for the following payload types:
    
    - PhoneNumber
    - SkypeCall
    - Url
    - Bookmark
    - CalendarEvent

--- QRCoderTests/PayloadGeneratorTests.cs
+++ QRCoderTests/PayloadGeneratorTests.cs
@@ -441,7 +441,7 @@ namespace QRCoderTests
         [Category("PayloadGenerator/Geolocation")]
         public void geolocation_should_escape_input()
         {
-            var latitude = "51,227741";
+            var latitude = "51,227741"; 
             var longitude = "6,773456";
             var encoding = PayloadGenerator.Geolocation.GeolocationEncoding.GEO;
 
@@ -463,6 +463,181 @@ namespace QRCoderTests
             generator.ToString().ShouldBe("geo:51.227741,6.773456");
         }
 
+
+        [Fact]
+        [Category("PayloadGenerator/PhoneNumber")]
+        public void phonenumber_should_build()
+        {
+            var number = "+495321123456";
+
+            var generator = new PayloadGenerator.PhoneNumber(number);
+
+            generator.ToString().ShouldBe("tel:+495321123456");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Skype")]
+        public void skype_should_build()
+        {
+            var username = "johndoe123";
+
+            var generator = new PayloadGenerator.SkypeCall(username);
+
+            generator.ToString().ShouldBe("skype:johndoe123?call");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Url")]
+        public void url_should_build_http()
+        {
+            var url = "http://code-bude.net";
+
+            var generator = new PayloadGenerator.Url(url);
+
+            generator.ToString().ShouldBe("http://code-bude.net");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Url")]
+        public void url_should_build_https()
+        {
+            var url = "https://code-bude.net";
+
+            var generator = new PayloadGenerator.Url(url);
+
+            generator.ToString().ShouldBe("https://code-bude.net");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Url")]
+        public void url_should_add_http()
+        {
+            var url = "code-bude.net";
+
+            var generator = new PayloadGenerator.Url(url);
+
+            generator.ToString().ShouldBe("http://code-bude.net");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Bookmark")]
+        public void bookmark_should_build()
+        {
+            var url = "http://code-bude.net";
+            var title = "A nerd's blog";
+
+            var generator = new PayloadGenerator.Bookmark(url, title);
+
+            generator.ToString().ShouldBe("MEBKM:TITLE:A nerd's blog;URL:http\\://code-bude.net;;");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/Bookmark")]
+        public void bookmark_should_escape_input()
+        {
+            var url = "http://code-bude.net/fake,url.html";
+            var title = "A nerd's blog: \\All;the;things\\";
+
+            var generator = new PayloadGenerator.Bookmark(url, title);
+
+            generator.ToString().ShouldBe("MEBKM:TITLE:A nerd's blog\\: \\\\All\\;the\\;things\\\\;URL:http\\://code-bude.net/fake\\,url.html;;");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/CalendarEvent")]
+        public void calendarevent_should_build_universal()
+        {
+            var subject = "Release party";
+            var description = "A small party for the new QRCoder. Bring some beer!";
+            var location = "Programmer's paradise, Beachtown, Paradise";
+            var alldayEvent = false;
+            var begin = new DateTime(2016,01,03,12,00,00);
+            var end = new DateTime(2016,01,03,14,30,0);
+            var encoding = PayloadGenerator.CalendarEvent.EventEncoding.Universal;
+
+            var generator = new PayloadGenerator.CalendarEvent(subject, description, location, begin, end, alldayEvent, encoding);
+
+            generator.ToString().ShouldBe($"BEGIN:VEVENT{Environment.NewLine}SUMMARY:Release party{Environment.NewLine}DESCRIPTION:A small party for the new QRCoder. Bring some beer!{Environment.NewLine}LOCATION:Programmer's paradise, Beachtown, Paradise{Environment.NewLine}DTSTART:20160103T120000{Environment.NewLine}DTEND:20160103T143000{Environment.NewLine}END:VEVENT");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/CalendarEvent")]
+        public void calendarevent_should_build_ical()
+        {
+            var subject = "Release party";
+            var description = "A small party for the new QRCoder. Bring some beer!";
+            var location = "Programmer's paradise, Beachtown, Paradise";
+            var alldayEvent = false;
+            var begin = new DateTime(2016, 01, 03, 12, 00, 00);
+            var end = new DateTime(2016, 01, 03, 14, 30, 0);
+            var encoding = PayloadGenerator.CalendarEvent.EventEncoding.iCalComplete;
+
+            var generator = new PayloadGenerator.CalendarEvent(subject, description, location, begin, end, alldayEvent, encoding);
+
+            generator.ToString().ShouldBe($"BEGIN:VCALENDAR{Environment.NewLine}VERSION:2.0{Environment.NewLine}BEGIN:VEVENT{Environment.NewLine}SUMMARY:Release party{Environment.NewLine}DESCRIPTION:A small party for the new QRCoder. Bring some beer!{Environment.NewLine}LOCATION:Programmer's paradise, Beachtown, Paradise{Environment.NewLine}DTSTART:20160103T120000{Environment.NewLine}DTEND:20160103T143000{Environment.NewLine}END:VEVENT{Environment.NewLine}END:VCALENDAR");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/CalendarEvent")]
+        public void calendarevent_should_build_allday()
+        {
+            var subject = "Release party";
+            var description = "A small party for the new QRCoder. Bring some beer!";
+            var location = "Programmer's paradise, Beachtown, Paradise";
+            var alldayEvent = true;
+            var begin = new DateTime(2016, 01, 03);
+            var end = new DateTime(2016, 01, 03);
+            var encoding = PayloadGenerator.CalendarEvent.EventEncoding.Universal;
+
+            var generator = new PayloadGenerator.CalendarEvent(subject, description, location, begin, end, alldayEvent, encoding);
+
+            generator.ToString().ShouldBe($"BEGIN:VEVENT{Environment.NewLine}SUMMARY:Release party{Environment.NewLine}DESCRIPTION:A small party for the new QRCoder. Bring some beer!{Environment.NewLine}LOCATION:Programmer's paradise, Beachtown, Paradise{Environment.NewLine}DTSTART:20160103{Environment.NewLine}DTEND:20160103{Environment.NewLine}END:VEVENT");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/CalendarEvent")]
+        public void calendarevent_should_care_empty_fields()
+        {
+            var subject = "Release party";
+            var description = "";
+            var location = string.Empty;
+            var alldayEvent = false;
+            var begin = new DateTime(2016, 01, 03, 12, 00, 00);
+            var end = new DateTime(2016, 01, 03, 14, 30, 0);
+            var encoding = PayloadGenerator.CalendarEvent.EventEncoding.Universal;
+
+            var generator = new PayloadGenerator.CalendarEvent(subject, description, location, begin, end, alldayEvent, encoding);
+
+            generator.ToString().ShouldBe($"BEGIN:VEVENT{Environment.NewLine}SUMMARY:Release party{Environment.NewLine}DTSTART:20160103T120000{Environment.NewLine}DTEND:20160103T143000{Environment.NewLine}END:VEVENT");
+        }
+
+
+        [Fact]
+        [Category("PayloadGenerator/CalendarEvent")]
+        public void calendarevent_should_add_unused_params()
+        {
+            var subject = "Release party";
+            var description = "A small party for the new QRCoder. Bring some beer!";
+            var location = "Programmer's paradise, Beachtown, Paradise";
+            var alldayEvent = false;
+            var begin = new DateTime(2016, 01, 03, 12, 00, 00);
+            var end = new DateTime(2016, 01, 03, 14, 30, 0);
+
+            var generator = new PayloadGenerator.CalendarEvent(subject, description, location, begin, end, alldayEvent);
+
+            generator.ToString().ShouldBe($"BEGIN:VEVENT{Environment.NewLine}SUMMARY:Release party{Environment.NewLine}DESCRIPTION:A small party for the new QRCoder. Bring some beer!{Environment.NewLine}LOCATION:Programmer's paradise, Beachtown, Paradise{Environment.NewLine}DTSTART:20160103T120000{Environment.NewLine}DTEND:20160103T143000{Environment.NewLine}END:VEVENT");
+        }
     }
 }
+
 
