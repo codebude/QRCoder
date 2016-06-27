450a6a00675afffa2b1d42d88e382ce3637e00d7
codebude <netblognet@googlemail.com>
2016-06-27 13:50:50 +0200

    Added Base64 qr code format
    Added Base64QRCode.cs class, which enables to generate qr codes in
    base64 format.

---
+++ QRCoder/Base64QRCode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace QRCoder
+{
+    public class Base64QRCode : AbstractQRCode<string>, IDisposable
+    {
+        private QRCode qr;
+
+        public Base64QRCode(QRCodeData data) : base(data) {
+            qr = new QRCode(data);
+        }
+        
+        public override string GetGraphic(int pixelsPerModule)
+        {
+            return this.GetGraphic(pixelsPerModule, Color.Black, Color.White, true);
+        }
+                
+
+        public string GetGraphic(int pixelsPerModule, string darkColorHtmlHex, string lightColorHtmlHex, bool drawQuietZones = true)
+        {
+            return this.GetGraphic(pixelsPerModule, ColorTranslator.FromHtml(darkColorHtmlHex), ColorTranslator.FromHtml(lightColorHtmlHex), drawQuietZones);
+        }
+
+        public string GetGraphic(int pixelsPerModule, Color darkColor, Color lightColor, bool drawQuietZones = true)
+        {
+            Bitmap bmp = qr.GetGraphic(pixelsPerModule, darkColor, lightColor, drawQuietZones);
+            return BitmapToBase64(bmp);
+        }
+
+        public string GetGraphic(int pixelsPerModule, Color darkColor, Color lightColor, Bitmap icon, int iconSizePercent = 15, int iconBorderWidth = 6, bool drawQuietZones = true)
+        {
+            Bitmap bmp = qr.GetGraphic(pixelsPerModule, darkColor, lightColor, icon, iconSizePercent, iconBorderWidth, drawQuietZones);
+            return BitmapToBase64(bmp);
+        }
+        
+
+        private string BitmapToBase64(Bitmap bmp)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            bmp.Save(memoryStream, ImageFormat.Png);
+            byte[] bitmapBytes = memoryStream.GetBuffer();
+            string bitmapString = Convert.ToBase64String(bitmapBytes, Base64FormattingOptions.InsertLineBreaks);
+            return bitmapString;
+        }
+
+        public void Dispose()
+        {
+            this.QrCodeData = null;
+        }
+
+    }
+}
