2aab9c65b6e42cca126089a9707ff134f93ec87a
Raffael Herrmann <netblognet@googlemail.com>
2016-09-10 17:42:28 +0200

    Updated build scripts

--- MyGet.bat
+++ MyGet.bat
@@ -16,6 +16,6 @@ if not "%PackageVersion%" == "" (
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
-mkdir Build\lib\netcore45
+mkdir Build\lib\netcore
 
 %NuGet% pack "QRCoder\QRCoder.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
