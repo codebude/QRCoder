070b0e5d73e601a2032d92b729c14bca76afd960
Raffael Herrmann <netblognet@googlemail.com>
2016-12-09 23:45:47 +0100

    Added Girocode to PayloadGenerator

--- QRCoder/PayloadGenerator.cs
+++ QRCoder/PayloadGenerator.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace QRCoder
 {
@@ -309,6 +312,116 @@ namespace QRCoder
             }
         }
 
+        public class Girocode
+        {
+            //Keep in mind, that the ECC level has to be set to "M" when generating a Girocode!
+            //Girocode specification: http://www.europeanpaymentscouncil.eu/index.cfm/knowledge-bank/epc-documents/quick-response-code-guidelines-to-enable-data-capture-for-the-initiation-of-a-sepa-credit-transfer/epc069-12-quick-response-code-guidelines-to-enable-data-capture-for-the-initiation-of-a-sepa-credit-transfer1/
+
+            private string br = "\n";
+            private readonly string iban, bic, name, purposeOfCreditTransfer, remittanceInformation, messageToGirocodeUser;
+            private readonly decimal amount;
+            private readonly GirocodeVersion version;
+            private readonly GirocodeEncoding encoding;
+            private readonly TypeOfRemittance typeOfRemittance;
+            
+
+            public Girocode(string iban, string bic, string name, decimal amount, string remittanceInformation = "", TypeOfRemittance typeOfRemittance = TypeOfRemittance.Unstructured, string purposeOfCreditTransfer = "", string messageToGirocodeUser = "", GirocodeVersion version = GirocodeVersion.Version1, GirocodeEncoding encoding = GirocodeEncoding.ISO_8859_1)
+            {
+                this.version = version;
+                this.encoding = encoding;
+                if (!Regex.IsMatch(iban.Replace(" ",""), @"[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}"))
+                    throw new GirocodeException("The IBAN entered isn't valid.");
+                this.iban = iban.Replace(" ","").ToUpper();
+                if (!Regex.IsMatch(bic.Replace(" ", ""), @"([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)"))
+                    throw new GirocodeException("The BIC entered isn't valid.");
+                this.bic = bic.Replace(" ", "").ToUpper();
+                if (name.Length > 70)
+                    throw new GirocodeException("(Payee-)Name must be shorter than 71 chars.");
+                this.name = name;
+                if (amount.ToString().Replace(",", ".").Contains(".") && amount.ToString().Replace(",",".").Split('.')[1].TrimEnd('0').Length > 2)
+                    throw new GirocodeException("Amount must have less than 3 digits after decimal point.");
+                if (amount < 0.01m || amount > 999999999.99m)
+                    throw new GirocodeException("Amount has to at least 0.01 and must be smaller or equal to 999999999.99.");
+                this.amount = amount;
+                if (purposeOfCreditTransfer.Length > 4)
+                    throw new GirocodeException("Purpose of credit transfer can only have 4 chars at maximum.");
+                this.purposeOfCreditTransfer = purposeOfCreditTransfer;
+                if (typeOfRemittance.Equals(TypeOfRemittance.Unstructured) && remittanceInformation.Length > 140)
+                    throw new GirocodeException("Unstructured reference texts have to shorter than 141 chars.");
+                if (typeOfRemittance.Equals(TypeOfRemittance.Structured) && remittanceInformation.Length > 35)
+                    throw new GirocodeException("Structured reference texts have to shorter than 36 chars.");
+                this.typeOfRemittance = typeOfRemittance;
+                this.remittanceInformation = remittanceInformation;
+                if (messageToGirocodeUser.Length > 70)
+                    throw new GirocodeException("Message to the Girocode-User reader texts have to shorter than 71 chars.");
+                this.messageToGirocodeUser = messageToGirocodeUser;
+
+            }
+
+            public override string ToString()
+            {
+
+                var girocodePayload = "BCD" + br;
+                girocodePayload += (version.Equals(GirocodeVersion.Version1) ? "001" : "002") + br;
+                girocodePayload += (int)encoding + 1 + br;
+                girocodePayload += "SCT" + br;
+                girocodePayload += bic + br;
+                girocodePayload += name + br;
+                girocodePayload += iban + br;
+                girocodePayload += $"EUR{amount:0.00}".Replace(",",".") + br;
+                girocodePayload += purposeOfCreditTransfer + br;
+                girocodePayload += (typeOfRemittance.Equals(TypeOfRemittance.Structured)
+                    ? remittanceInformation
+                    : string.Empty) + br;
+                girocodePayload += (typeOfRemittance.Equals(TypeOfRemittance.Unstructured)
+                    ? remittanceInformation
+                    : string.Empty) + br;
+                girocodePayload += messageToGirocodeUser;
+
+                return ConvertStringToEncoding(girocodePayload, encoding.ToString().Replace("_","-"));
+            }
+
+            public enum GirocodeVersion
+            {
+                Version1,
+                Version2
+            }
+
+            public enum TypeOfRemittance
+            {
+                Structured,
+                Unstructured
+            }
+
+            public enum GirocodeEncoding
+            {
+                UTF_8,
+                ISO_8859_1, 
+                ISO_8859_2,
+                ISO_8859_4,
+                ISO_8859_5,
+                ISO_8859_7,
+                ISO_8859_10,
+                ISO_8859_15
+            }
+
+            public class GirocodeException : Exception
+            {
+                public GirocodeException()
+                {
+                }
+
+                public GirocodeException(string message)
+                    : base(message)
+                {
+                }
+
+                public GirocodeException(string message, Exception inner)
+                    : base(message, inner)
+                {
+                }
+            }
+        }
 
         public class CalendarEvent
         {
@@ -347,6 +460,15 @@ namespace QRCoder
                 iCalComplete,
                 Universal
             }
+        }
+
+        private static string ConvertStringToEncoding(string message, string encoding)
+        {
+            Encoding iso = Encoding.GetEncoding(encoding);
+            Encoding utf8 = Encoding.UTF8;
+            byte[] utfBytes = utf8.GetBytes(message);
+            byte[] isoBytes = Encoding.Convert(utf8, iso, utfBytes);
+            return iso.GetString(isoBytes);
         }
 
         private static string EscapeInput(string inp, bool simple = false)
