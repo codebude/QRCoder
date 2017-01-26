e159f251b7c076d77d5c68530aa609a6b81d9a9d
codebude <netblognet@googlemail.com>
2017-01-26 15:24:48 +0100

    Added crc32 check

--- MyGet.bat
+++ MyGet.bat
@@ -11,12 +11,16 @@ if not "%PackageVersion%" == "" (
 
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild" QRCoderPackageBuild.sln /p:Configuration="%config%";VisualStudioVersion=14.0 /tv:14.0 /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
+crc32 bin\$configuration$\net35\QRCoder.dll
+crc32 bin\$configuration$\net40\QRCoder.dll
+crc32 bin\$configuration$\netcore\QRCoder.dll
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net35
 mkdir Build\lib\net40
 mkdir Build\lib\portable-net45+netcore45+wpa81+wp81+wp8+uap
+mkdir Build\lib\uap10.0
 mkdir Build\lib\xamarinios
 mkdir Build\lib\monoandroid
 mkdir Build\lib\monotouch
