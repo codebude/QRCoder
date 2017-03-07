febddc8f4a72e9e21fb0f8508b335ee65a35daf2
Matthew Vukomanovic <mvukomanovic@sateva.com.au>
2017-03-07 13:58:58 +0800

    Include comments for ECCLevel

--- QRCoder/QRCodeGenerator.cs
+++ QRCoder/QRCodeGenerator.cs
@@ -1197,11 +1197,26 @@ namespace QRCoder
             }
         }
 
+        /// <summary>
+        /// Error correction level. These define the tolerance levels for how much of the code can be lost before the code cannot be recovered.
+        /// </summary>
         public enum ECCLevel
         {
+            /// <summary>
+            /// 7% may be lost before recovery is not possible
+            /// </summary>
             L,
+            /// <summary>
+            /// 15% may be lost before recovery is not possible
+            /// </summary>
             M,
+            /// <summary>
+            /// 25% may be lost before recovery is not possible
+            /// </summary>
             Q,
+            /// <summary>
+            /// 30% may be lost before recovery is not possible
+            /// </summary>
             H
         }
 
