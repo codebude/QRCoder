50735e27d6a2cf73b6bcd37a0deb5c230534a8f1
codebude <netblognet@googlemail.com>
2017-03-21 09:42:58 +0100

    Added monero payload generator

--- QRCoder/PayloadGenerator.cs
+++ QRCoder/PayloadGenerator.cs
@@ -706,7 +706,53 @@ namespace QRCoder
         }
 
 
+        public class MoneroTransaction
+        {
+            private readonly string address, txPaymentId, recipientName, txDescription;
+            private readonly float? txAmount;
+                       
 
+            public MoneroTransaction(string address, float? txAmount = null, string txPaymentId = null, string recipientName = null, string txDescription = null)
+            {
+                if (string.IsNullOrEmpty(address))
+                    throw new MoneroTransactionException("The address is mandatory and has to be set.");
+                this.address = address;
+                if (txAmount != null && txAmount <= 0)
+                    throw new MoneroTransactionException("Value of 'txAmount' must be greater than 0.");
+                this.txAmount = txAmount;
+                this.txPaymentId = txPaymentId;
+                this.recipientName = recipientName;
+                this.txDescription = txDescription;
+            }
+
+            public override string ToString()
+            {
+                var moneroUri = $"monero://{address}{(!string.IsNullOrEmpty(txPaymentId) || !string.IsNullOrEmpty(recipientName) || !string.IsNullOrEmpty(txDescription) || txAmount != null ? "?" : string.Empty)}";
+                moneroUri += (!string.IsNullOrEmpty(txPaymentId) ? $"tx_payment_id={Uri.EscapeDataString(txPaymentId)}&" : string.Empty);
+                moneroUri += (!string.IsNullOrEmpty(recipientName) ? $"recipient_name={Uri.EscapeDataString(recipientName)}&" : string.Empty);
+                moneroUri += (txAmount != null ? $"tx_amount={txAmount.ToString().Replace(",",".")}&" : string.Empty);
+                moneroUri += (!string.IsNullOrEmpty(txDescription) ? $"tx_description={Uri.EscapeDataString(txDescription)}" : string.Empty);
+                return moneroUri.TrimEnd('&');
+            }
+                        
+
+            public class MoneroTransactionException : Exception
+            {
+                public MoneroTransactionException()
+                {
+                }
+
+                public MoneroTransactionException(string message)
+                    : base(message)
+                {
+                }
+
+                public MoneroTransactionException(string message, Exception inner)
+                    : base(message, inner)
+                {
+                }
+            }
+        }
 
 
 
