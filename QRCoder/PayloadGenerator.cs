cc2add4c2f8b2d4e9909c70560f9d45d36663ad5
Raffael Herrmann <netblognet@googlemail.com>
2017-05-14 20:42:29 +0200

    Added ContactData payload generator
    Added payload generator for contacts in vCard (2.1, 3.0 & 4.0) and
    MeCard format.

--- QRCoder/PayloadGenerator.cs
+++ QRCoder/PayloadGenerator.cs
@@ -357,6 +357,180 @@ namespace QRCoder
             }
         }
 
+        public class ContactData
+        {
+            private readonly string firstname;
+            private readonly string lastname;
+            private readonly string nickname;
+            private readonly string phone;
+            private readonly string mobilePhone;
+            private readonly string workPhone;
+            private readonly string email;
+            private readonly DateTime? birthday;
+            private readonly string website;
+            private readonly string street;
+            private readonly string houseNumber;
+            private readonly string city;
+            private readonly string zipCode;
+            private readonly string country;
+            private readonly string note;
+            private readonly ContactOutputType outputType;
+
+
+            /// <summary>
+            /// Generates a vCard or meCard contact dataset
+            /// </summary>
+            /// <param name="outputType">Payload output type</param>
+            /// <param name="firstname">The firstname</param>
+            /// <param name="lastname">The lastname</param>
+            /// <param name="nickname">The displayname</param>
+            /// <param name="phone">Normal phone number</param>
+            /// <param name="mobilePhone">Mobile phone</param>
+            /// <param name="workPhone">Office phone number</param>
+            /// <param name="email">E-Mail address</param>
+            /// <param name="birthday">Birthday</param>
+            /// <param name="website">Website / Homepage</param>
+            /// <param name="street">Street</param>
+            /// <param name="houseNumber">Housenumber</param>
+            /// <param name="city">City</param>
+            /// <param name="zipCode">Zip code</param>
+            /// <param name="country">Country</param>
+            /// <param name="note">Memo text / notes</param>            
+            public ContactData(ContactOutputType outputType, string firstname, string lastname, string nickname = null, string phone = null, string mobilePhone = null, string workPhone = null, string email = null, DateTime? birthday = null, string website = null, string street = null, string houseNumber = null, string city = null, string zipCode = null, string country = null, string note = null)
+            {             
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.nickname = nickname;
+                this.phone = phone;
+                this.mobilePhone = mobilePhone;
+                this.workPhone = workPhone;
+                this.email = email;
+                this.birthday = birthday;
+                this.website = website;
+                this.street = street;
+                this.houseNumber = houseNumber;
+                this.city = city;
+                this.zipCode = zipCode;
+                this.country = country;
+                this.note = note;
+                this.outputType = outputType;
+            }
+
+            public override string ToString()
+            {
+                string payload = string.Empty;
+                if (outputType.Equals(ContactOutputType.MeCard))
+                {
+                    payload += "MECARD+\r\n";
+                    if (!string.IsNullOrEmpty(firstname) && !string.IsNullOrEmpty(lastname))
+                        payload += $"N:{lastname}, {firstname}\r\n";
+                    else if (!string.IsNullOrEmpty(firstname) || !string.IsNullOrEmpty(lastname))
+                        payload += $"N:{firstname}{lastname}\r\n";
+                    if (!string.IsNullOrEmpty(phone))
+                        payload += $"TEL:{phone}\r\n";
+                    if (!string.IsNullOrEmpty(mobilePhone))
+                        payload += $"TEL:{mobilePhone}\r\n";
+                    if (!string.IsNullOrEmpty(workPhone))
+                        payload += $"TEL:{workPhone}\r\n";
+                    if (!string.IsNullOrEmpty(email))
+                        payload += $"EMAIL:{email}\r\n";
+                    if (!string.IsNullOrEmpty(note))
+                        payload += $"NOTE:{note}\r\n";
+                    if (birthday != null)
+                        payload += $"BDAY:{((DateTime)birthday).ToString("yyyyMMdd")}\r\n";                    
+                    payload += $"ADR:,,{(!string.IsNullOrEmpty(street) ? street+" " : "")}{(!string.IsNullOrEmpty(houseNumber)?houseNumber:"")},{(!string.IsNullOrEmpty(city) ? city : "")},,{(!string.IsNullOrEmpty(zipCode) ? zipCode : "")},{(!string.IsNullOrEmpty(country) ? country : "")}\r\n";
+                    if (!string.IsNullOrEmpty(phone))
+                        payload += $"URL:{website}\r\n";
+                    if (!string.IsNullOrEmpty(nickname))
+                        payload += $"NICKNAME:{nickname}\r\n";
+                    payload = payload.Trim(new char[] { '\r', '\n' });
+                }
+                else
+                {
+                    var version = outputType.ToString().Substring(5);
+                    if (version.Length > 1)
+                        version = version.Insert(1, ".");
+                    else
+                        version += ".0";
+
+                    payload += "BEGIN:VCARD\r\n";
+                    payload += $"VERSION:{version}\r\n";
+
+                    payload += $"N:{(!string.IsNullOrEmpty(lastname) ? lastname : "")};{(!string.IsNullOrEmpty(firstname) ? firstname : "")};;;\r\n";
+                    payload += $"FN:{(!string.IsNullOrEmpty(firstname) ? firstname + " " : "")}{(!string.IsNullOrEmpty(lastname) ? lastname : "")}\r\n";
+                    
+                    if (!string.IsNullOrEmpty(phone))
+                    {
+                        payload += $"TEL;";
+                        if (outputType.Equals(ContactOutputType.VCard21))
+                            payload += $"HOME;VOICE:{phone}";
+                        else if (outputType.Equals(ContactOutputType.VCard3))
+                            payload += $"TYPE=HOME,VOICE:{phone}";
+                        else
+                            payload += $"TYPE=home,voice;VALUE=uri:tel:{phone}";
+                        payload += "\r\n";
+                    }
+                    if (!string.IsNullOrEmpty(workPhone))
+                    {
+                        payload += $"TEL;";
+                        if (outputType.Equals(ContactOutputType.VCard21))
+                            payload += $"WORK;VOICE:{workPhone}";
+                        else if (outputType.Equals(ContactOutputType.VCard3))
+                            payload += $"TYPE=WORK,VOICE:{workPhone}";
+                        else
+                            payload += $"TYPE=work,voice;VALUE=uri:tel:{workPhone}";
+                        payload += "\r\n";
+                    }
+                    if (!string.IsNullOrEmpty(mobilePhone))
+                    {
+                        payload += $"TEL;";
+                        if (outputType.Equals(ContactOutputType.VCard21))
+                            payload += $"HOME;CELL:{mobilePhone}";
+                        else if (outputType.Equals(ContactOutputType.VCard3))
+                            payload += $"TYPE=HOME,CELL:{mobilePhone}";
+                        else
+                            payload += $"TYPE=home,cell;VALUE=uri:tel:{mobilePhone}";
+                        payload += "\r\n";
+                    }
+
+                    payload += "ADR;";
+                    if (outputType.Equals(ContactOutputType.VCard21))
+                        payload += "HOME;PREF:";
+                    else if (outputType.Equals(ContactOutputType.VCard3))
+                        payload += "TYPE=HOME,PREF:";
+                    else
+                        payload += "TYPE=home,pref:";
+                    payload += $";;{(!string.IsNullOrEmpty(street) ? street + " " : "")}{(!string.IsNullOrEmpty(houseNumber) ? houseNumber : "")};{(!string.IsNullOrEmpty(city) ? city : "")};;{(!string.IsNullOrEmpty(zipCode) ? zipCode : "")};{(!string.IsNullOrEmpty(country) ? country : "")}\r\n";
+                    
+                    if (birthday != null)
+                        payload += $"BDAY:{((DateTime)birthday).ToString("yyyyMMdd")}\r\n";
+                    if (!string.IsNullOrEmpty(phone))
+                        payload += $"URL:{website}\r\n";
+                    if (!string.IsNullOrEmpty(email))
+                        payload += $"EMAIL:{email}\r\n";
+                    if (!string.IsNullOrEmpty(note))
+                        payload += $"NOTE:{note}\r\n";
+                    if (!outputType.Equals(ContactOutputType.VCard21) && !string.IsNullOrEmpty(nickname))
+                        payload += $"NICKNAME:{nickname}\r\n";                    
+
+                    payload += "END:VCARD";
+                }
+                
+                return payload;
+            }
+
+            /// <summary>
+            /// Possible output types. Either vCard 2.1, vCard 3.0, vCard 4.0 or MeCard.
+            /// </summary>
+            public enum ContactOutputType
+            {
+                MeCard,
+                VCard21,
+                VCard3,
+                VCard4
+            }
+        }
+
         public class BitcoinAddress
         {
             private readonly string address, label, message;
--- QRCoderTests/PayloadGeneratorTests.cs
+++ QRCoderTests/PayloadGeneratorTests.cs
@@ -2381,6 +2381,133 @@ namespace QRCoderTests
 
 
         [Fact]
+        [Category("PayloadGenerator/ContactData")]
+        public void contactdata_generator_can_generate_payload_simple_mecard()
+        {
+            var firstname = "John";
+            var lastname = "Doe";
+            var outputType = PayloadGenerator.ContactData.ContactOutputType.MeCard;
+
+            var generator = new PayloadGenerator.ContactData(outputType, firstname, lastname);
+
+            generator
+                .ToString()
+                .ShouldBe("MECARD+\r\nN:Doe, John\r\nADR:,,,,,,");
+        }
+
+        [Fact]
+        [Category("PayloadGenerator/ContactData")]
+        public void contactdata_generator_can_generate_payload_full_mecard()
+        {
+            var firstname = "John";
+            var lastname = "Doe";
+            var nickname = "Johnny";
+            var phone = "+4253212222";
+            var mobilePhone = "+421701234567";
+            var workPhone = "+4253211337";
+            var email = "me@john.doe";
+            var birthday = new DateTime(1970,02,01);
+            var website = "http://john.doe";
+            var street = "Long street";
+            var houseNumber = "42";
+            var city = "Super-Town";
+            var zipCode = "12345";
+            var country = "Starlight Country";
+            var note = "Badass programmer.";
+            var outputType = PayloadGenerator.ContactData.ContactOutputType.MeCard;
+
+            var generator = new PayloadGenerator.ContactData(outputType, firstname, lastname, nickname, phone, mobilePhone, workPhone, email, birthday, website, street, houseNumber, city, zipCode, country, note);
+
+            generator
+                .ToString()
+                .ShouldBe("MECARD+\r\nN:Doe, John\r\nTEL:+4253212222\r\nTEL:+421701234567\r\nTEL:+4253211337\r\nEMAIL:me@john.doe\r\nNOTE:Badass programmer.\r\nBDAY:19700201\r\nADR:,,Long street 42,Super-Town,,12345,Starlight Country\r\nURL:http://john.doe\r\nNICKNAME:Johnny");
+        }
+
+        [Fact]
+        [Category("PayloadGenerator/ContactData")]
+        public void contactdata_generator_can_generate_payload_full_vcard21()
+        {
+            var firstname = "John";
+            var lastname = "Doe";
+            var nickname = "Johnny";
+            var phone = "+4253212222";
+            var mobilePhone = "+421701234567";
+            var workPhone = "+4253211337";
+            var email = "me@john.doe";
+            var birthday = new DateTime(1970, 02, 01);
+            var website = "http://john.doe";
+            var street = "Long street";
+            var houseNumber = "42";
+            var city = "Super-Town";
+            var zipCode = "12345";
+            var country = "Starlight Country";
+            var note = "Badass programmer.";
+            var outputType = PayloadGenerator.ContactData.ContactOutputType.VCard21;
+
+            var generator = new PayloadGenerator.ContactData(outputType, firstname, lastname, nickname, phone, mobilePhone, workPhone, email, birthday, website, street, houseNumber, city, zipCode, country, note);
+
+            generator
+                .ToString()
+                .ShouldBe("BEGIN:VCARD\r\nVERSION:2.1\r\nN:Doe;John;;;\r\nFN:John Doe\r\nTEL;HOME;VOICE:+4253212222\r\nTEL;WORK;VOICE:+4253211337\r\nTEL;HOME;CELL:+421701234567\r\nADR;HOME;PREF:;;Long street 42;Super-Town;;12345;Starlight Country\r\nBDAY:19700201\r\nURL:http://john.doe\r\nEMAIL:me@john.doe\r\nNOTE:Badass programmer.\r\nEND:VCARD");
+        }
+
+        [Fact]
+        [Category("PayloadGenerator/ContactData")]
+        public void contactdata_generator_can_generate_payload_full_vcard3()
+        {
+            var firstname = "John";
+            var lastname = "Doe";
+            var nickname = "Johnny";
+            var phone = "+4253212222";
+            var mobilePhone = "+421701234567";
+            var workPhone = "+4253211337";
+            var email = "me@john.doe";
+            var birthday = new DateTime(1970, 02, 01);
+            var website = "http://john.doe";
+            var street = "Long street";
+            var houseNumber = "42";
+            var city = "Super-Town";
+            var zipCode = "12345";
+            var country = "Starlight Country";
+            var note = "Badass programmer.";
+            var outputType = PayloadGenerator.ContactData.ContactOutputType.VCard3;
+
+            var generator = new PayloadGenerator.ContactData(outputType, firstname, lastname, nickname, phone, mobilePhone, workPhone, email, birthday, website, street, houseNumber, city, zipCode, country, note);
+
+            generator
+                .ToString()
+                .ShouldBe("BEGIN:VCARD\r\nVERSION:3.0\r\nN:Doe;John;;;\r\nFN:John Doe\r\nTEL;TYPE=HOME,VOICE:+4253212222\r\nTEL;TYPE=WORK,VOICE:+4253211337\r\nTEL;TYPE=HOME,CELL:+421701234567\r\nADR;TYPE=HOME,PREF:;;Long street 42;Super-Town;;12345;Starlight Country\r\nBDAY:19700201\r\nURL:http://john.doe\r\nEMAIL:me@john.doe\r\nNOTE:Badass programmer.\r\nNICKNAME:Johnny\r\nEND:VCARD");
+        }
+
+        [Fact]
+        [Category("PayloadGenerator/ContactData")]
+        public void contactdata_generator_can_generate_payload_full_vcard4()
+        {
+            var firstname = "John";
+            var lastname = "Doe";
+            var nickname = "Johnny";
+            var phone = "+4253212222";
+            var mobilePhone = "+421701234567";
+            var workPhone = "+4253211337";
+            var email = "me@john.doe";
+            var birthday = new DateTime(1970, 02, 01);
+            var website = "http://john.doe";
+            var street = "Long street";
+            var houseNumber = "42";
+            var city = "Super-Town";
+            var zipCode = "12345";
+            var country = "Starlight Country";
+            var note = "Badass programmer.";
+            var outputType = PayloadGenerator.ContactData.ContactOutputType.VCard4;
+
+            var generator = new PayloadGenerator.ContactData(outputType, firstname, lastname, nickname, phone, mobilePhone, workPhone, email, birthday, website, street, houseNumber, city, zipCode, country, note);
+
+            generator
+                .ToString()
+                .ShouldBe("BEGIN:VCARD\r\nVERSION:4.0\r\nN:Doe;John;;;\r\nFN:John Doe\r\nTEL;TYPE=home,voice;VALUE=uri:tel:+4253212222\r\nTEL;TYPE=work,voice;VALUE=uri:tel:+4253211337\r\nTEL;TYPE=home,cell;VALUE=uri:tel:+421701234567\r\nADR;TYPE=home,pref:;;Long street 42;Super-Town;;12345;Starlight Country\r\nBDAY:19700201\r\nURL:http://john.doe\r\nEMAIL:me@john.doe\r\nNOTE:Badass programmer.\r\nNICKNAME:Johnny\r\nEND:VCARD");
+        }
+
+        [Fact]
         [Category("PayloadGenerator/WhatsAppMessage")]
         public void whatsapp_generator_can_generate_payload_simple()
         {
