205ed389d4c1f501ced6ecaf4ca061b1c43ae916
Navid Matin <navid@thelevelup.com>
2017-06-23 15:07:23 -0400

    Fixed the issue with number of characters per module be bigger than 1

--- QRCoder/ASCIIQRCode.cs
+++ QRCoder/ASCIIQRCode.cs
@@ -18,35 +18,48 @@ namespace QRCoder
 
         public string[] GetLineByLineGraphic(int characterPerModule)
         {
+            return GetLineByLineGraphic(characterPerModule, "██", "  ");
+        }
+
+        public string GetGraphic(int characterPerModule, string darkColorString, string whiteSpaceString, string endOfLine = "\n")
+        {
+            return String.Join(endOfLine, GetLineByLineGraphic(characterPerModule, darkColorString, whiteSpaceString));
+        }
+        /// <summary>
+        /// Returns an array of strings that contain each line of the resulting QR code
+        /// </summary>
+        /// <param name="characterPerModule">number of characters per module.</param>
+        /// <param name="darkColorString">string or character for use as dark color bits</param>
+        /// <param name="whiteSpaceString">string or character for use as white space bits</param>
+        /// <returns></returns>
+        public string[] GetLineByLineGraphic(int characterPerModule, string darkColorString, string whiteSpaceString)
+        {
             var qrCode = new List<string>();
             var sideLength = QrCodeData.ModuleMatrix.Count * characterPerModule;
 
-            for (var y = 0; y < sideLength; y = y + characterPerModule)
+            for (var y = 0; y < sideLength; y++)
             {
                 bool emptyLine = true;
                 var lineBuilder = new StringBuilder();
-                for (var cm = 0; cm < characterPerModule; cm++)
-                {
-                    for (var x = sideLength - 1; x >= 0; x = x - characterPerModule)
-                    {
-                        var module =
-                            QrCodeData.ModuleMatrix[(x + characterPerModule) / characterPerModule - 1][
-                                (y + characterPerModule) / characterPerModule - 1];
-                        for (var i = 0; i < characterPerModule; i++)
-                        {
-                            lineBuilder.Append(module ? "██" : "  ");
-                        }
-                        if (module)
-                        {
-                            emptyLine = false;
-                        }
 
-                    }
-                    if (!emptyLine)
+                for (var x = 0; x < QrCodeData.ModuleMatrix.Count; x++)
+                {
+                    var module = QrCodeData.ModuleMatrix[x][(y + characterPerModule) / characterPerModule - 1];
+
+                    for (var i = 0; i < characterPerModule; i++)
                     {
-                        qrCode.Add(lineBuilder.ToString());
+                        lineBuilder.Append(module ? darkColorString : whiteSpaceString);
+                    }
+                    if (module)
+                    {
+                        emptyLine = false;
                     }
                 }
+                if (!emptyLine)
+                {
+                    qrCode.Add(lineBuilder.ToString());
+                }
+
             }
             return qrCode.ToArray();
         }
