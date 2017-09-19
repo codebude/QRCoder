a4e831f65d7adf1c553b4658eae3b1c5deac44a8
Filip Kassovic <kassovic.f@st-software.com>
2017-09-19 09:00:40 +0200

    - renamed drawQuietZones to drawBorder

--- QRCoder/SvgQRCode.cs
+++ QRCoder/SvgQRCode.cs
@@ -14,35 +14,35 @@ namespace QRCoder
             var viewBox = new Size(pixelsPerModule*this.QrCodeData.ModuleMatrix.Count, pixelsPerModule * this.QrCodeData.ModuleMatrix.Count);
             return this.GetGraphic(viewBox, Color.Black, Color.White);
         }
-        public string GetGraphic(int pixelsPerModule, Color foregroundColor, Color backgroundColor, bool drawQuietZones = true)
+        public string GetGraphic(int pixelsPerModule, Color foregroundColor, Color backgroundColor, bool drawBorder = true)
         {
             var viewBox = new Size(pixelsPerModule * this.QrCodeData.ModuleMatrix.Count, pixelsPerModule * this.QrCodeData.ModuleMatrix.Count);
-            return this.GetGraphic(viewBox, foregroundColor, backgroundColor, drawQuietZones);
+            return this.GetGraphic(viewBox, foregroundColor, backgroundColor, drawBorder);
         }
 
-        public string GetGraphic(int pixelsPerModule, string foregroundColorHex, string backgroundColorHex, bool drawQuietZones = true)
+        public string GetGraphic(int pixelsPerModule, string foregroundColorHex, string backgroundColorHex, bool drawBorder = true)
         {
             var viewBox = new Size(pixelsPerModule * this.QrCodeData.ModuleMatrix.Count, pixelsPerModule * this.QrCodeData.ModuleMatrix.Count);
-            return this.GetGraphic(viewBox, foregroundColorHex, backgroundColorHex, drawQuietZones);
+            return this.GetGraphic(viewBox, foregroundColorHex, backgroundColorHex, drawBorder);
         }
 
-        public string GetGraphic(Size viewBox, bool drawQuietZones = true)
+        public string GetGraphic(Size viewBox, bool drawBorder = true)
         {
-            return this.GetGraphic(viewBox, Color.Black, Color.White, drawQuietZones);
+            return this.GetGraphic(viewBox, Color.Black, Color.White, drawBorder);
         }
 
-        public string GetGraphic(Size viewBox, Color foregroundColor, Color backgroundColor, bool drawQuietZones = true)
+        public string GetGraphic(Size viewBox, Color foregroundColor, Color backgroundColor, bool drawBorder = true)
         {
-            return this.GetGraphic(viewBox, ColorTranslator.ToHtml(Color.FromArgb(foregroundColor.ToArgb())), ColorTranslator.ToHtml(Color.FromArgb(backgroundColor.ToArgb())), drawQuietZones);
+            return this.GetGraphic(viewBox, ColorTranslator.ToHtml(Color.FromArgb(foregroundColor.ToArgb())), ColorTranslator.ToHtml(Color.FromArgb(backgroundColor.ToArgb())), drawBorder);
         }
 
-        public string GetGraphic(Size viewBox, string foregroundColorHex, string backgroundColorHex, bool drawQuietZones = true)
+        public string GetGraphic(Size viewBox, string foregroundColorHex, string backgroundColorHex, bool drawBorder = true)
         {
             
-			int matrixWidth = this.QrCodeData.ModuleMatrix.Count - (drawQuietZones ? 0 : 8);
+			int matrixWidth = this.QrCodeData.ModuleMatrix.Count - (drawBorder ? 0 : 8);
 			decimal preciseUnitsPerModule = Convert.ToDecimal(Math.Min(viewBox.Width, viewBox.Height)) / (decimal)matrixWidth;
 			decimal unitsPerModule = Decimal.Floor(preciseUnitsPerModule * 100m) / 100m; //takes first 2 decimal places (rounds down - cuts off remaining decimal places). This makes sure, that resulting SVG doesn't overflow requested size.
-            var matrixOffset = drawQuietZones ? 0 : 4;
+            var matrixOffset = drawBorder ? 0 : 4;
 
 			var svgFile = new StringBuilder($"<svg version=\"1.1\" baseProfile=\"full\" width=\"{viewBox.Width}\" height=\"{viewBox.Height}\" xmlns=\"http://www.w3.org/2000/svg\">");
 			svgFile.AppendLine($"<rect x=\"0\" y=\"0\" width=\"{matrixWidth * unitsPerModule}\" height=\"{matrixWidth * unitsPerModule}\" fill=\"{backgroundColorHex}\" />");
