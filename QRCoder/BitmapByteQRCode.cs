641d79fd427191fc980948db2b782fdd2f496c68
sbrickey <me@sbrickey.net>
2018-06-18 15:57:24 -0400

    add BitmapByteQrCodeUtility.GetBmpQrCode()
    add an easy-to-call one-hit wrapper to generate bitmap image byte array

--- QRCoder/BitmapByteQRCode.cs
+++ QRCoder/BitmapByteQRCode.cs
@@ -97,4 +97,16 @@ namespace QRCoder
             return bytes;
         }
     }
+    
+  
+    public sealed class BitmapByteQrCodeUtility
+    {
+        public static byte[] GetBmpQrCode(string txt, QRCodeGenerator.ECCLevel eccLevel, int size)
+        {
+            using (var qrGen = new QRCodeGenerator())
+            using (var qrCode = qrGen.CreateQrCode(txt, eccLevel))
+            using (var qrBmp = new BitmapByteQRCode(qrCode))
+                return qrBmp.GetGraphic(size);
+        }
+    }
 }
