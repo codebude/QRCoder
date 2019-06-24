da89291a6646c0be046382646e184bac08e48e84
codebude <netblognet@googlemail.com>
2019-06-24 11:47:45 +0200

    Added exception handling for too long data

---
+++ QRCoder/Exceptions/DataTooLongException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QRCoder.Exceptions
+{
+    public class DataTooLongException : Exception
+    {
+        public DataTooLongException(string eccLevel, string encodingMode, int maxSizeByte) : base(
+            $"The given payload exceeds the maximum size of the QR code standard. The maximum size allowed for the choosen paramters (ECC level={eccLevel}, EncodingMode={encodingMode}) is {maxSizeByte} byte."
+        ){}
+    }
+}
