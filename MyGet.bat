0d084052bc8957fa12766deddc465903afe8c4f3
Curt Sturm <csturm83@users.noreply.github.com>
2019-07-06 08:59:22 -0500

    Update csproj for NuGet packaging via dotnet pack.

--- MyGet.bat
+++ MyGet.bat
@@ -3,58 +3,28 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
-set version=1.3.7
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
 
 echo Working dir: %cd%
 
-echo Create template folders
+dotnet clean -c %config%
 
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net35
-mkdir Build\lib\net40
-mkdir Build\lib\netcore
-mkdir Build\lib\netstandard2.0
-
-echo Compile single projects
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" QRCoder\QRCoder.csproj /p:Configuration="%config%";VisualStudioVersion=15.0 /tv:15.0 /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false /t:Rebuild
-copy "QRCoder\bin\%config%\net35\*.dll" "Build\lib\net35"
-del /F /S /Q "QRCoder\bin"
-del /F /S /Q "QRCoder\obj"
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" QRCoder\QRCoder.NET40.csproj /p:Configuration="%config%";VisualStudioVersion=15.0 /tv:15.0 /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false /t:Rebuild
-copy "QRCoder\bin\%config%\net40\*.dll" "Build\lib\net40"
-del /F /S /Q "QRCoder\bin"
-del /F /S /Q "QRCoder\obj"
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" QRCoder\QRCoderProject.Portable.csproj /p:Configuration="%config%";VisualStudioVersion=15.0 /tv:15.0 /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false /t:Rebuild
-copy "QRCoder\bin\%config%\netcore\*.dll" "Build\lib\netcore"
-del /F /S /Q "QRCoder\bin"
-del /F /S /Q "QRCoder\obj"
-
-dotnet build /p:Configuration="%config%" QRCoder\QRCoder.NETCore20.csproj
-copy "QRCoder\bin\%config%\netstandard2.0\*.dll" "Build\lib\netstandard2.0"
-del /F /S /Q "QRCoder\bin"
-del /F /S /Q "QRCoder\obj"
-
+dotnet build -c %config%
 
 echo Assembly information
 
-powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\Build\lib\net35\QRCoder.dll\").ImageRuntimeVersion"
-certUtil -hashfile "Build\lib\net35\QRCoder.dll" md5
+powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\QRCoder\bin\%config%\net35\QRCoder.dll\").ImageRuntimeVersion"
+certUtil -hashfile "QRCoder\bin\%config%\net35\QRCoder.dll" md5
 
-powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\Build\lib\net40\QRCoder.dll\").ImageRuntimeVersion"
-certUtil -hashfile "Build\lib\net40\QRCoder.dll" md5
+powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\QRCoder\bin\%config%\net40\QRCoder.dll\").ImageRuntimeVersion"
+certUtil -hashfile "QRCoder\bin\%config%\net40\QRCoder.dll" md5
 
-powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\Build\lib\netcore\QRCoder.dll\").ImageRuntimeVersion"
-certUtil -hashfile "Build\lib\netcore\QRCoder.dll" md5
+powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\QRCoder\bin\%config%\net45\QRCoder.dll\").ImageRuntimeVersion"
+certUtil -hashfile "QRCoder\bin\%config%\net45\QRCoder.dll" md5
 
-powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\Build\lib\netstandard2.0\QRCoder.dll\").ImageRuntimeVersion"
-certUtil -hashfile "Build\lib\netstandard2.0\QRCoder.dll" md5
+powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\QRCoder\bin\%config%\netstandard1.1\QRCoder.dll\").ImageRuntimeVersion"
+certUtil -hashfile "QRCoder\bin\%config%\netstandard1.1\QRCoder.dll" md5
 
-call %NuGet% pack "QRCoder.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+powershell -Command "[Reflection.Assembly]::ReflectionOnlyLoadFrom(\"%cd%\QRCoder\bin\%config%\netstandard2.0\QRCoder.dll\").ImageRuntimeVersion"
+certUtil -hashfile "QRCoder\bin\%config%\netstandard2.0\QRCoder.dll" md5
+
+dotnet pack -c %config% --no-build
