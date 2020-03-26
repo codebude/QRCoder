1c4e203e52fbdfc6f0f6757264fb0843dddb73cd
Raffael Herrmann <netblognet@googlemail.com>
2020-03-26 12:15:42 +0100

    Correct alphaNumEncTable testcase
    Corrected the test case in correlation to the changes in https://github.com/codebude/QRCoder/pull/194/commits/2fd3d6ebaf75d777f277125f35102960bf0a3217

--- QRCoderTests/QRGeneratorTests.cs
+++ QRCoderTests/QRGeneratorTests.cs
@@ -40,7 +40,7 @@ namespace QRCoderTests
             {
                 checkString += $"{listitem.Key},{listitem.Value}:";
             }
-            checkString.ShouldBe("0,0:1,1:2,2:3,3:4,4:5,5:6,6:7,7:8,8:9,9:A,10:B,11:C,12:D,13:E,14:F,15:G,16:H,17:I,18:J,19:K,20:L,21:M,22:N,23:O,24:P,25:Q,26:R,27:S,28:T,29:U,30:V,31:W,32:X,33:Y,34:Z,35: ,36:$,37:%,38:*,39:+,40:-,41:.,42:/,43::,44:");
+            checkString.ShouldBe(" ,0:$,1:%,2:*,3:+,4:-,5:.,6:/,7::,8:");
         }
 
         [Fact]
