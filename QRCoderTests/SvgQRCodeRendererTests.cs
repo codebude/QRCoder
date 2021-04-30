16d4abe5f9a7cf5669c177fafec171a614e11843
codebude <netblognet@googlemail.com>
2021-04-30 16:23:38 +0200

    Create SvgQRCodeRendererTests.cs

---
+++ QRCoderTests/SvgQRCodeRendererTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using QRCoder;
+using Shouldly;
+using QRCoderTests.XUnitExtenstions;
+using System.IO;
+using System.Security.Cryptography;
+#if !NETCOREAPP1_1
+using System.Drawing;
+#endif
+
+namespace QRCoderTests
+{
+
+    public class SvgQRCodeRendererTests
+    {
+
+
+#if !NETCOREAPP1_1 && !NETCOREAPP2_0
+
+        [Fact]
+        [Category("QRRenderer/QRCode")]
+        public void can_create_qrcode_with_transparent_logo_graphic()
+        {        
+            //Create QR code
+            var gen = new QRCodeGenerator();
+            var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.H);
+            var svg = new SvgQRCode(data).GetGraphic(10, Color.Red, Color.White);
+
+            var md5 = new MD5CryptoServiceProvider();
+            var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(svg));
+            var result = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+            result.ShouldBe("0ad8bc75675d04ba0caff51c7a89992c");
+        }
+#endif
+    }
+}
+
+
+
