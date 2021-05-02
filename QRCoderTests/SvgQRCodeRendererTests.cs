e58ee4469dfd7a60b34220783fb11e54cc6d545f
Raffael Herrmann <netblognet@googlemail.com>
2021-05-02 11:31:16 +0200

    Added test case for SvgQRCode without quietzones

--- QRCoderTests/SvgQRCodeRendererTests.cs
+++ QRCoderTests/SvgQRCodeRendererTests.cs
@@ -19,8 +19,8 @@ namespace QRCoderTests
 #if !NETCOREAPP1_1 && !NETCOREAPP2_0
 
         [Fact]
-        [Category("QRRenderer/QRCode")]
-        public void can_create_qrcode_with_transparent_logo_graphic()
+        [Category("QRRenderer/SvgQRCode")]
+        public void can_render_svg_qrcode()
         {        
             //Create QR code
             var gen = new QRCodeGenerator();
@@ -32,6 +32,22 @@ namespace QRCoderTests
             var result = BitConverter.ToString(hash).Replace("-", "").ToLower();
 
             result.ShouldBe("0ad8bc75675d04ba0caff51c7a89992c");
+        }
+
+        [Fact]
+        [Category("QRRenderer/SvgQRCode")]
+        public void can_render_svg_qrcode_without_quietzones()
+        {
+            //Create QR code
+            var gen = new QRCodeGenerator();
+            var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.H);
+            var svg = new SvgQRCode(data).GetGraphic(10, Color.Red, Color.White, false);
+
+            var md5 = new MD5CryptoServiceProvider();
+            var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(svg));
+            var result = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+            result.ShouldBe("24392f47d4c1c2c5097bd6b3f8eefccc");
         }
 #endif
     }
