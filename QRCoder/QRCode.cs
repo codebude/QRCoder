05a1e530407c95861613f3e59b59145821fbadbd
Raffael Herrmann <netblognet@googlemail.com>
2021-11-23 22:46:59 +0100

    Fixed icon-border/-background rendering

--- QRCoder/QRCode.cs
+++ QRCoder/QRCode.cs
@@ -58,7 +58,7 @@ namespace QRCoder
             return bmp;
         }
 
-        public Bitmap GetGraphic(int pixelsPerModule, Color darkColor, Color lightColor, Bitmap icon=null, int iconSizePercent=15, int iconBorderWidth = 6, bool drawQuietZones = true)
+        public Bitmap GetGraphic(int pixelsPerModule, Color darkColor, Color lightColor, Bitmap icon=null, int iconSizePercent=15, int iconBorderWidth = 0, bool drawQuietZones = true, Color? iconBackgroundColor = null)
         {
             var size = (this.QrCodeData.ModuleMatrix.Count - (drawQuietZones ? 0 : 8)) * pixelsPerModule;
             var offset = drawQuietZones ? 0 : 4 * pixelsPerModule;
@@ -72,36 +72,34 @@ namespace QRCoder
                 gfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
                 gfx.CompositingQuality = CompositingQuality.HighQuality;
                 gfx.Clear(lightColor);
-
                 var drawIconFlag = icon != null && iconSizePercent > 0 && iconSizePercent <= 100;
-
-                GraphicsPath iconPath = null;
-                float iconDestWidth = 0, iconDestHeight = 0, iconX = 0, iconY = 0;
-
-                if (drawIconFlag)
-                {
-                    iconDestWidth = iconSizePercent * bmp.Width / 100f;
-                    iconDestHeight = drawIconFlag ? iconDestWidth * icon.Height / icon.Width : 0;
-                    iconX = (bmp.Width - iconDestWidth) / 2;
-                    iconY = (bmp.Height - iconDestHeight) / 2;
-
-                    var centerDest = new RectangleF(iconX - iconBorderWidth, iconY - iconBorderWidth, iconDestWidth + iconBorderWidth * 2, iconDestHeight + iconBorderWidth * 2);
-                    iconPath = this.CreateRoundedRectanglePath(centerDest, iconBorderWidth * 2);
-                }
-
+                               
                 for (var x = 0; x < size + offset; x = x + pixelsPerModule)
                 {
                     for (var y = 0; y < size + offset; y = y + pixelsPerModule)
                     {
                         var moduleBrush = this.QrCodeData.ModuleMatrix[(y + pixelsPerModule) / pixelsPerModule - 1][(x + pixelsPerModule) / pixelsPerModule - 1] ? darkBrush : lightBrush;
-
                         gfx.FillRectangle(moduleBrush , new Rectangle(x - offset, y - offset, pixelsPerModule, pixelsPerModule));
                     }
                 }
 
                 if (drawIconFlag)
                 {
+                    float iconDestWidth = iconSizePercent * bmp.Width / 100f;
+                    float iconDestHeight = drawIconFlag ? iconDestWidth * icon.Height / icon.Width : 0;
+                    float iconX = (bmp.Width - iconDestWidth) / 2;
+                    float iconY = (bmp.Height - iconDestHeight) / 2;
+                    var centerDest = new RectangleF(iconX - iconBorderWidth, iconY - iconBorderWidth, iconDestWidth + iconBorderWidth * 2, iconDestHeight + iconBorderWidth * 2);
                     var iconDestRect = new RectangleF(iconX, iconY, iconDestWidth, iconDestHeight);
+                    var iconBgBrush = iconBackgroundColor != null ? new SolidBrush((Color)iconBackgroundColor) : lightBrush;
+                    //Only render icon/logo background, if iconBorderWith is set > 0
+                    if (iconBorderWidth > 0)
+                    {                        
+                        using (GraphicsPath iconPath = CreateRoundedRectanglePath(centerDest, iconBorderWidth * 2))
+                        {                            
+                            gfx.FillPath(iconBgBrush, iconPath);
+                        }
+                    }
                     gfx.DrawImage(icon, iconDestRect, new RectangleF(0, 0, icon.Width, icon.Height), GraphicsUnit.Pixel);
                 }
 
@@ -129,7 +127,7 @@ namespace QRCoder
 
     public static class QRCodeHelper
     {
-        public static Bitmap GetQRCode(string plainText, int pixelsPerModule, Color darkColor, Color lightColor, ECCLevel eccLevel, bool forceUtf8 = false, bool utf8BOM = false, EciMode eciMode = EciMode.Default, int requestedVersion = -1, Bitmap icon = null, int iconSizePercent = 15, int iconBorderWidth = 6, bool drawQuietZones = true)
+        public static Bitmap GetQRCode(string plainText, int pixelsPerModule, Color darkColor, Color lightColor, ECCLevel eccLevel, bool forceUtf8 = false, bool utf8BOM = false, EciMode eciMode = EciMode.Default, int requestedVersion = -1, Bitmap icon = null, int iconSizePercent = 15, int iconBorderWidth = 0, bool drawQuietZones = true)
         {
             using (var qrGenerator = new QRCodeGenerator())
             using (var qrCodeData = qrGenerator.CreateQrCode(plainText, eccLevel, forceUtf8, utf8BOM, eciMode, requestedVersion))
