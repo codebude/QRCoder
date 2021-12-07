8bffc630a5c461a5eda558d7c071765860d5fe4e
Raffael Herrmann <netblognet@googlemail.com>
2021-12-07 17:18:17 +0100

    Add XamlQRCode test cases

--- QRCoderTests/Helpers/HelperFunctions.cs
+++ QRCoderTests/Helpers/HelperFunctions.cs
@@ -1,17 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
 #if !NETCOREAPP1_1
 using System.Drawing;
 #endif
+#if NETFRAMEWORK || NET5_0_WINDOWS
+using SW = System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+#endif
+
 
 namespace QRCoderTests.Helpers
 {
     public static class HelperFunctions
     {
+
+#if NETFRAMEWORK || NET5_0_WINDOWS
+        public static BitmapSource ToBitmapSource(DrawingImage source)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawImage(source, new SW.Rect(new SW.Point(0, 0), new SW.Size(source.Width, source.Height)));
+            drawingContext.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)source.Width, (int)source.Height, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+            return bmp;
+        }
+
+        public static Bitmap BitmapSourceToBitmap(DrawingImage xamlImg)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(ToBitmapSource(xamlImg)));
+                encoder.Save(ms);
+
+                using (Bitmap bmp = new Bitmap(ms))
+                {
+                    return new Bitmap(bmp);
+                }
+            }
+        }
+#endif 
 
 #if !NETCOREAPP1_1
         public static string GetAssemblyPath()
