4e88511005796138b3a7a9b8e63839f7a4fe84cc
Michael Haren <mharen@overdrive.com>
2023-02-27 12:48:47 -0500

    url payload generator should detect all-caps protocol

--- QRCoder/PayloadGenerator.cs
+++ QRCoder/PayloadGenerator.cs
@@ -311,7 +311,7 @@ namespace QRCoder
             private readonly string url;
 
             /// <summary>
-            /// Generates a link. If not given, http/https protocol will be added.
+            /// Generates a link. If protocol not given, http protocol will be added.
             /// </summary>
             /// <param name="url">Link url target</param>
             public Url(string url)
@@ -321,7 +321,7 @@ namespace QRCoder
 
             public override string ToString()
             {
-                return (!this.url.StartsWith("http") ? "http://" + this.url : this.url);
+                return (!this.url.StartsWith("http", StringComparison.OrdinalIgnoreCase) ? "http://" + this.url : this.url);
             }
         }
 
