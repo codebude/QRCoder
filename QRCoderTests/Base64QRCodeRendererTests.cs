c9b102b2a6b0b3d1154967708b15f816b4a040b4
Shane32 <shane@acdmail.com>
2024-04-13 15:01:41 -0400

    update

--- QRCoderTests/Base64QRCodeRendererTests.cs
+++ QRCoderTests/Base64QRCodeRendererTests.cs
@@ -1,24 +1,14 @@
 ﻿#if !NETCOREAPP1_1
-using Xunit;
 using QRCoder;
-using Shouldly;
 using QRCoderTests.Helpers.XUnitExtenstions;
-using QRCoderTests.Helpers;
+using Shouldly;
 using System;
-
 using System.Drawing;
 using System.IO;
-using System.Security.Policy;
+using Xunit;
 
 namespace QRCoderTests
 {
-    /****************************************************************************************************
-     * Note: Test cases compare the outcome visually even if it's slower than a byte-wise compare.
-     *       This is necessary, because the Deflate implementation differs on the different target
-     *       platforms and thus the outcome, even if visually identical, differs. Thus only a visual
-     *       test method makes sense. In addition bytewise differences shouldn't be important, if the
-     *       visual outcome is identical and thus the qr code is identical/scannable.
-     ****************************************************************************************************/
     public class Base64QRCodeRendererTests
     {
         private readonly QRCodeData data;
