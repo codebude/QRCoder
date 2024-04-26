ffe2461a5dc8a4884b8f1f0cab4c48c07ef88cd2
Raffael Herrmann <netblognet@googlemail.com>
2024-04-27 00:24:59 +0200

    Raised version counter to 1.5.1

--- QRCoder.Xaml/Properties/AssemblyInfo.cs
+++ QRCoder.Xaml/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
