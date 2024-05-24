3edab65adf707cef87eb95a328f8a0e93e0266df
Shane32 <shane@acdmail.com>
2024-05-23 23:06:52 -0400

    Refactor StringExtensions

--- QRCoder/Extensions/StringExtensions.cs
+++ QRCoder/Extensions/StringExtensions.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace QRCoder
 {
-    internal static class String40Methods
+    internal static class StringExtensions
     {
         /// <summary>
         /// Indicates whether the specified string is null, empty, or consists only of white-space characters.
@@ -10,14 +8,14 @@ namespace QRCoder
         /// <returns>
         ///   <see langword="true"/> if the <paramref name="value"/> is null, empty, or white space; otherwise, <see langword="false"/>.
         /// </returns>
-        public static bool IsNullOrWhiteSpace(String value)
+        public static bool IsNullOrWhiteSpace(this string value)
         {
 #if NET35
             if (value == null) return true;
 
             for (int i = 0; i < value.Length; i++)
             {
-                if (!Char.IsWhiteSpace(value[i])) return false;
+                if (!char.IsWhiteSpace(value[i])) return false;
             }
 
             return true;
