f490971e9e65abc81f29fdacd53668b98ce11901
Shane32 <shane@acdmail.com>
2024-06-04 20:49:25 -0400

    File-scoped namespace

--- QRCoderDemo/Form1.cs
+++ QRCoderDemo/Form1.cs
@@ -1,161 +1,154 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 using QRCoder;
-using System.Drawing.Imaging;
-using System.IO;
 
-namespace QRCoderDemo
+namespace QRCoderDemo;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void Form1_Load(object sender, EventArgs e)
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        comboBoxECC.SelectedIndex = 0; //Pre-select ECC level "L"
+        RenderQrCode();
+    }
+
+    private void buttonGenerate_Click(object sender, EventArgs e)
+    {
+        RenderQrCode();
+    }
+
+    private void RenderQrCode()
+    {
+        string level = comboBoxECC.SelectedItem.ToString();
+        var eccLevel = (QRCodeGenerator.ECCLevel)(level == "L" ? 0 : level == "M" ? 1 : level == "Q" ? 2 : 3);
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(textBoxQRCode.Text, eccLevel);
+        using var qrCode = new QRCode(qrCodeData);
+        pictureBoxQRCode.BackgroundImage = qrCode.GetGraphic(20, GetPrimaryColor(), GetBackgroundColor(),
+            GetIconBitmap(), (int)iconSize.Value);
+
+        pictureBoxQRCode.Size = new System.Drawing.Size(pictureBoxQRCode.Width, pictureBoxQRCode.Height);
+        //Set the SizeMode to center the image.
+        pictureBoxQRCode.SizeMode = PictureBoxSizeMode.CenterImage;
+
+        pictureBoxQRCode.SizeMode = PictureBoxSizeMode.StretchImage;
+    }
+
+    private Bitmap GetIconBitmap()
+    {
+        if (iconPath.Text.Length == 0)
         {
-            comboBoxECC.SelectedIndex = 0; //Pre-select ECC level "L"
+            return null;
+        }
+        try
+        {
+            return new Bitmap(iconPath.Text);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    private void selectIconBtn_Click(object sender, EventArgs e)
+    {
+        var openFileDlg = new OpenFileDialog();
+        openFileDlg.Title = "Select icon";
+        openFileDlg.Multiselect = false;
+        openFileDlg.CheckFileExists = true;
+        if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            iconPath.Text = openFileDlg.FileName;
+            if (iconSize.Value == 0)
+            {
+                iconSize.Value = 15;
+            }
+        }
+        else
+        {
+            iconPath.Text = "";
+        }
+    }
+
+
+    private void btn_save_Click(object sender, EventArgs e)
+    {
+
+        // Displays a SaveFileDialog so the user can save the Image
+        var saveFileDialog1 = new SaveFileDialog();
+        saveFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|JPeg Image|*.jpg|Gif Image|*.gif";
+        saveFileDialog1.Title = "Save an Image File";
+        saveFileDialog1.ShowDialog();
+
+        // If the file name is not an empty string open it for saving.
+        if (saveFileDialog1.FileName != "")
+        {
+            // Saves the Image via a FileStream created by the OpenFile method.
+            using var fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+            // Saves the Image in the appropriate ImageFormat based upon the
+            // File type selected in the dialog box.
+            // NOTE that the FilterIndex property is one-based.
+
+            ImageFormat imageFormat = null;
+            imageFormat = saveFileDialog1.FilterIndex switch
+            {
+                1 => ImageFormat.Bmp,
+                2 => ImageFormat.Png,
+                3 => ImageFormat.Jpeg,
+                4 => ImageFormat.Gif,
+                _ => throw new NotSupportedException("File extension is not supported"),
+            };
+            pictureBoxQRCode.BackgroundImage.Save(fs, imageFormat);
+        }
+    }
+
+    public void ExportToBmp(string path)
+    {
+
+    }
+
+    private void textBoxQRCode_TextChanged(object sender, EventArgs e)
+    {
+        RenderQrCode();
+    }
+
+    private void comboBoxECC_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        RenderQrCode();
+    }
+
+    private void panelPreviewPrimaryColor_Click(object sender, EventArgs e)
+    {
+        if (colorDialogPrimaryColor.ShowDialog() == DialogResult.OK)
+        {
+            panelPreviewPrimaryColor.BackColor = colorDialogPrimaryColor.Color;
             RenderQrCode();
         }
+    }
 
-        private void buttonGenerate_Click(object sender, EventArgs e)
+    private void panelPreviewBackgroundColor_Click(object sender, EventArgs e)
+    {
+        if (colorDialogBackgroundColor.ShowDialog() == DialogResult.OK)
         {
+            panelPreviewBackgroundColor.BackColor = colorDialogBackgroundColor.Color;
             RenderQrCode();
         }
+    }
 
-        private void RenderQrCode()
-        {
-            string level = comboBoxECC.SelectedItem.ToString();
-            var eccLevel = (QRCodeGenerator.ECCLevel)(level == "L" ? 0 : level == "M" ? 1 : level == "Q" ? 2 : 3);
-            using var qrGenerator = new QRCodeGenerator();
-            using var qrCodeData = qrGenerator.CreateQrCode(textBoxQRCode.Text, eccLevel);
-            using var qrCode = new QRCode(qrCodeData);
-            pictureBoxQRCode.BackgroundImage = qrCode.GetGraphic(20, GetPrimaryColor(), GetBackgroundColor(),
-                GetIconBitmap(), (int)iconSize.Value);
+    private Color GetPrimaryColor()
+    {
+        return panelPreviewPrimaryColor.BackColor;
+    }
 
-            pictureBoxQRCode.Size = new System.Drawing.Size(pictureBoxQRCode.Width, pictureBoxQRCode.Height);
-            //Set the SizeMode to center the image.
-            pictureBoxQRCode.SizeMode = PictureBoxSizeMode.CenterImage;
-
-            pictureBoxQRCode.SizeMode = PictureBoxSizeMode.StretchImage;
-        }
-
-        private Bitmap GetIconBitmap()
-        {
-            if (iconPath.Text.Length == 0)
-            {
-                return null;
-            }
-            try
-            {
-                return new Bitmap(iconPath.Text);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        private void selectIconBtn_Click(object sender, EventArgs e)
-        {
-            var openFileDlg = new OpenFileDialog();
-            openFileDlg.Title = "Select icon";
-            openFileDlg.Multiselect = false;
-            openFileDlg.CheckFileExists = true;
-            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                iconPath.Text = openFileDlg.FileName;
-                if (iconSize.Value == 0)
-                {
-                    iconSize.Value = 15;
-                }
-            }
-            else
-            {
-                iconPath.Text = "";
-            }
-        }
-
-
-        private void btn_save_Click(object sender, EventArgs e)
-        {
-
-            // Displays a SaveFileDialog so the user can save the Image
-            var saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|JPeg Image|*.jpg|Gif Image|*.gif";
-            saveFileDialog1.Title = "Save an Image File";
-            saveFileDialog1.ShowDialog();
-
-            // If the file name is not an empty string open it for saving.
-            if (saveFileDialog1.FileName != "")
-            {
-                // Saves the Image via a FileStream created by the OpenFile method.
-                using var fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
-                // Saves the Image in the appropriate ImageFormat based upon the
-                // File type selected in the dialog box.
-                // NOTE that the FilterIndex property is one-based.
-
-                ImageFormat imageFormat = null;
-                imageFormat = saveFileDialog1.FilterIndex switch
-                {
-                    1 => ImageFormat.Bmp,
-                    2 => ImageFormat.Png,
-                    3 => ImageFormat.Jpeg,
-                    4 => ImageFormat.Gif,
-                    _ => throw new NotSupportedException("File extension is not supported"),
-                };
-                pictureBoxQRCode.BackgroundImage.Save(fs, imageFormat);
-            }
-        }
-
-        public void ExportToBmp(string path)
-        {
-
-        }
-
-        private void textBoxQRCode_TextChanged(object sender, EventArgs e)
-        {
-            RenderQrCode();
-        }
-
-        private void comboBoxECC_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            RenderQrCode();
-        }
-
-        private void panelPreviewPrimaryColor_Click(object sender, EventArgs e)
-        {
-            if (colorDialogPrimaryColor.ShowDialog() == DialogResult.OK)
-            {
-                panelPreviewPrimaryColor.BackColor = colorDialogPrimaryColor.Color;
-                RenderQrCode();
-            }
-        }
-
-        private void panelPreviewBackgroundColor_Click(object sender, EventArgs e)
-        {
-            if (colorDialogBackgroundColor.ShowDialog() == DialogResult.OK)
-            {
-                panelPreviewBackgroundColor.BackColor = colorDialogBackgroundColor.Color;
-                RenderQrCode();
-            }
-        }
-
-        private Color GetPrimaryColor()
-        {
-            return panelPreviewPrimaryColor.BackColor;
-        }
-
-        private Color GetBackgroundColor()
-        {
-            return panelPreviewBackgroundColor.BackColor;
-        }
+    private Color GetBackgroundColor()
+    {
+        return panelPreviewBackgroundColor.BackColor;
     }
 }
