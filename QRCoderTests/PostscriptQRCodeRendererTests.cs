5dfb6c3e582c639a3239d6f633cda3aa8893ed1f
Shane Krueger <shane@acdmail.com>
2025-10-06 00:29:09 -0400

    Make Postscript renderer deterministic

--- QRCoderTests/PostscriptQRCodeRendererTests.cs
+++ QRCoderTests/PostscriptQRCodeRendererTests.cs
@@ -9,7 +9,7 @@ public class PostscriptQRCodeRendererTests
         var gen = new QRCodeGenerator();
         var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.L);
         var ps = new PostscriptQRCode(data).GetGraphic(5);
-        RemoveCreationDate(ps).ShouldMatchApproved(x => x.NoDiff());
+        ps.ShouldMatchApproved(x => x.NoDiff());
     }
 
     [Fact]
@@ -19,7 +19,7 @@ public class PostscriptQRCodeRendererTests
         var gen = new QRCodeGenerator();
         var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.L);
         var ps = new PostscriptQRCode(data).GetGraphic(5, true);
-        RemoveCreationDate(ps).ShouldMatchApproved(x => x.NoDiff());
+        ps.ShouldMatchApproved(x => x.NoDiff());
     }
 
     [Fact]
@@ -29,7 +29,7 @@ public class PostscriptQRCodeRendererTests
         var gen = new QRCodeGenerator();
         var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.L);
         var ps = new PostscriptQRCode(data).GetGraphic(new Size(33, 33));
-        RemoveCreationDate(ps).ShouldMatchApproved(x => x.NoDiff());
+        ps.ShouldMatchApproved(x => x.NoDiff());
     }
 
     [Fact]
@@ -39,7 +39,7 @@ public class PostscriptQRCodeRendererTests
         var gen = new QRCodeGenerator();
         var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.L);
         var ps = new PostscriptQRCode(data).GetGraphic(new Size(50, 50), false);
-        RemoveCreationDate(ps).ShouldMatchApproved(x => x.NoDiff());
+        ps.ShouldMatchApproved(x => x.NoDiff());
     }
 
     [Fact]
@@ -49,15 +49,6 @@ public class PostscriptQRCodeRendererTests
         var gen = new QRCodeGenerator();
         var data = gen.CreateQrCode("This is a quick test! 123#?", QRCodeGenerator.ECCLevel.L);
         var ps = new PostscriptQRCode(data).GetGraphic(5, Color.Red, Color.Blue);
-        RemoveCreationDate(ps).ShouldMatchApproved(x => x.NoDiff());
-    }
-
-    private static string RemoveCreationDate(string text)
-    {
-        // Regex pattern to match lines that start with %%CreationDate: followed by any characters until the end of the line
-        string pattern = @"%%CreationDate:.*\r?\n?";
-
-        // Use Regex.Replace to remove matching lines
-        return Regex.Replace(text, pattern, string.Empty, RegexOptions.Multiline);
+        ps.ShouldMatchApproved(x => x.NoDiff());
     }
 }
