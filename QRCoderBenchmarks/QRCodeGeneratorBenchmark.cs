a533b644b54c3869fd4ef358df158e55a837e9a5
Shane Krueger <shane@acdmail.com>
2025-10-13 13:27:24 -0400

    Add benchmark for multi-mode QR code generation (#674)

--- QRCoderBenchmarks/QRCodeGeneratorBenchmark.cs
+++ QRCoderBenchmarks/QRCodeGeneratorBenchmark.cs
@@ -14,6 +14,14 @@ public class QRCodeGeneratorBenchmark
     }
 
     [Benchmark]
+    public void CreateQRCodeMultiMode()
+    {
+        var payload = new QRCoder.PayloadGenerator.Url("HTTPS://GITHUB.COM/Shane32/QRCoder/blob/F89AA90081F369983A9BA114E49CC6EBF0B2A7B1/QRCoder/Framework4.0Methods/Stream4Methods.cs");
+        var qrGenerator = new QRCoder.QRCodeGenerator();
+        _ = qrGenerator.CreateQrCode(payload, QRCoder.QRCodeGenerator.ECCLevel.L);
+    }
+
+    [Benchmark]
     public void CreateQRCodeLong()
     {
         var payload = new QRCoder.PayloadGenerator.Url("https://github.com/Shane32/QRCoder/blob/f89aa90081f369983a9ba114e49cc6ebf0b2a7b1/QRCoder/Framework4.0Methods/Stream4Methods.cs");
